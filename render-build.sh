a9a5f9f1b2ea382e2d3ecdfd90b1334e98840835
Evelyne UMUHIRE <eumuhire@bk.rw>
2025-02-20 15:54:47 +0200

    Fix Chrome binary issue for Render

--- render-build.sh
+++ render-build.sh
@@ -8,6 +8,6 @@ apt install -y google-chrome-stable
 # Install ChromeDriver
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}')
 wget -q "https://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
+sudo unzip chromedriver_linux64.zip
 mv chromedriver /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
